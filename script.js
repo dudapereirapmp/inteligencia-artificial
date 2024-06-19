f142f8e8fa47ea29fb328d32c1432b072dec9a63
dudapereirapmp <127161056+dudapereirapmp@users.noreply.github.com>
2024-06-18 21:24:04 -0300

    kkkkkk

--- script.js
+++ script.js
@@ -69,13 +69,70 @@ const perguntas =[
                                                                     afirmacao:"Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxilio e não resultado final."
                                                                     }
                                                                     ]
-                                                                    },
+                                                                  },
                                                                 ];
 
 
                                                                 let atual=0;
                                                                 let perguntaAtual;
                                                                 let historiaFinal= "";
+
+                                                                function mostraPergunta() {
+                                                                    if (atual>= perguntas.length){
+                                                                        mostraResultado();
+                                                                        return;
+                                                                        }
+                                                                        perguntaAtual = perguntas[atual];
+                                                                        caixaPerguntas.textContent = perguntaAtual.enunciado;
+                                                                        caixaAlternativas.textContent = "";
+                                                                        mostraAlternativa();
+                                                                    }
+
+                                                                    function mostraAlternativa (){
+                                                                        for(const alternativa of perguntaAtual.alternativa) {
+                                                                            const botaoAlternativas = document.createElement("button")
+                                                                            botaoAlternativas.textContent = alternativa.texto;
+                                                                            botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+                                                                            caixaAlternativas.appendChild(botaoAlternativas);
+                                                                        }
+                                                                    }
+
+                                                                    function respostaSelecionada(opcaoSelecionada) {
+                                                                        const afirmacoes = opcaoSelecionada.afirmacao;
+                                                                        historiaFinal += afirmacoes + " ";
+                                                                        atual++;
+                                                                        mostraPergunta();
+                                                                    }
+
+                                                                     function mostraResultado () {
+                                                                        caixaPerguntas.textContent ="em 2049...";
+                                                                        textoResultado.textContent = historiaFinal;
+                                                                        caixaAlternativas.textContent = "";
+                                                                        {
+
+                                                                            mostraPergunta();
+                                                                            const horas = document.getElementById('horas');
+                                                                            const minutos = document.getElementById('minutos');
+                                                                            const segundos = document.getElementById('segundos');
+
+                                                                            const relogio = setInterval(function time() {
+                                                                                let dateToday = new Date();
+                                                                                let hr = dateToday.getHours();
+                                                                                let min = dateToday.getMinutes();
+                                                                                let s = dateToday.getSeconds();
+
+                                                                                if (hr < 10) hr = '0' + hr;
+
+                                                                                if (min < 10) min = '0' + min;
+
+                                                                                if (s < 10) s = '0' + min;
+
+                                                                                horas.textContent = hr;
+                                                                                minutos. textContent = min;
+                                                                                segundos.textContent = s;
+
+                                                                            })
+
 
                                                                 
 ]
